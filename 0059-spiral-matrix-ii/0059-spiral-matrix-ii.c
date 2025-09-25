d9209df8da6de9a14154422793ca423fa2538338
Prateek Raj <100523472+PrateekRaj8125@users.noreply.github.com>
2025-09-25 12:40:47 +0530

    Time: 0 ms (100%), Space: 10.3 MB (22.63%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** generateMatrix(int n, int* returnSize, int** returnColumnSizes) {
+    *returnSize = n;
+    *returnColumnSizes = malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        (*returnColumnSizes)[i] = n;
+    }
+    int **matrix = malloc(n * sizeof(int*));
+    for (int i = 0; i < n; i++) {
+        matrix[i] = malloc(n * sizeof(int));
+    }
+    int num = 1;
+    int top = 0, bottom = n - 1;
+    int left = 0, right = n - 1;
+    while (top <= bottom && left <= right) {
+        for (int j = left; j <= right; j++)
+            matrix[top][j] = num++;
+        top++;
+        for (int i = top; i <= bottom; i++)
+            matrix[i][right] = num++;
+        right--;
+        if (top <= bottom) {
+            for (int j = right; j >= left; j--)
+                matrix[bottom][j] = num++;
+            bottom--;
+        }
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--)
+                matrix[i][left] = num++;
+            left++;
+        }
+    }
+    return matrix;
+}
