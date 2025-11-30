9219d8dda71f1d6bd745c3cb14caff28a474fea1
Prateek Raj <100523472+PrateekRaj8125@users.noreply.github.com>
2025-11-30 13:35:31 +0530

    Time: 0 ms (100%), Space: 9.5 MB (37.4%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.c
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int sumOfLeftLeaves(struct TreeNode* root) {
+    if(root==NULL)
+        return 0;
+    int sum=0;
+    if(root->left){
+        if(root->left->left==NULL&&root->left->right==NULL)
+            sum+=root->left->val;
+        else
+            sum+=sumOfLeftLeaves(root->left);
+    }
+    sum+=sumOfLeftLeaves(root->right);
+    return sum;
+}
