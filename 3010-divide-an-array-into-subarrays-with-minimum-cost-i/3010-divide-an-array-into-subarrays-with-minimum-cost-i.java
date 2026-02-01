90f1c9b406e0d8f2ea84ce8dbc51a257b900bf25
Prateek Raj <crajprateek@gmail.com>
2026-02-01 22:47:03 +0530

    Time: 1 ms (99.2%), Space: 45.3 MB (87.6%) - LeetHub

---
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minimumCost(int[] nums) {
+        int n=nums.length,ans=Integer.MAX_VALUE;
+        for(int i=1;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                ans=Math.min(ans,nums[0]+nums[i]+nums[j]);
+            }
+        }
+        return ans;
+    }
+}
