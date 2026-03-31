a548727b8e431d5fa45f833693db70f2ae8dd4b7
Prateek Raj <crajprateek@gmail.com>
2026-03-31 22:31:20 +0530

    Time: 389 ms (12.88%), Space: 0B (100%) - LeetHub

--- 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -4,4 +4,4 @@ FROM Employees
 WHERE salary < 30000
     AND manager_id NOT IN (SELECT employee_id FROM Employees)
     AND manager_id IS NOT NULL
-ORDER BY employee_id ASC
+ORDER BY employee_id ASC;
