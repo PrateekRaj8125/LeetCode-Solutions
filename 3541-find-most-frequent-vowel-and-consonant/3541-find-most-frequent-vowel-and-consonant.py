07341a62381e941bd350b8f6e706bbfaee1a2e1c
Prateek Raj <crajprateek@gmail.com>
2026-06-24 13:21:16 +0530

    Time: 4 ms (22.43%), Space: 19.3 MB (62.87%) - LeetHub

--- 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.py
+++ 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.py
@@ -1,11 +1,10 @@
 class Solution:
     def maxFreqSum(self, s: str) -> int:
-        con = 0
-        vow = 0
-        d_set = set(s)
+        con=0;vow=0
+        d_set=set(s)
         for i in d_set:
             if i in "aeiou":
-                vow = max(vow, s.count(i))
+                vow=max(vow,s.count(i))
             else:
-                con = max(con, s.count(i))
-        return con+vow 
+                con=max(con,s.count(i))
+        return con+vow
